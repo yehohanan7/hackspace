f710e1122aa0f8afb5fd81c03e893157183bd051
John Pradeep <yehohanan7@gmail.com>
2012-06-14 03:59:01 +0530

    refactors

--- src/com/hackspace/main.clj
+++ src/com/hackspace/main.clj
@@ -1,9 +1,9 @@
 (ns com.hackspace.main
+  (:use [clojure.tools.cli :only (cli)])
   (:gen-class ))
 
 (require '(com.hackspace.gateway.dropbox [dropbox :as dropbox]))
 (require '(com.hackspace.utils [fs :as fs]))
-(use 'clojure.tools.cli)
 
 
 (defn initialize []
@@ -14,9 +14,9 @@
 
 (defn -main [& command_args]
   (let [[options args banner] (cli command_args
-    ["-h" "-help" "Displays help." :default false, :flag true]
-    ["-p" "-provider" "the file hosting provider" :default 'dropbox]
-    ["-i" "-init" "initializes hackspace" :default false, :flag true]
+    ["-h" "--help" "Displays help." :default false]
+    ["-p" "--provider" "the file hosting provider" :default 'dropbox]
+    ["-i" "--init" "initializes hackspace" :default false, :flag true]
     )]
     (if (:help options)
       (println banner)
