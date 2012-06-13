6645a9e069db1cc035f3b16ace6677e499a7b820
John Pradeep <yehohanan7@gmail.com>
2012-06-14 04:25:29 +0530

    wip

--- src/com/hackspace/main.clj
+++ src/com/hackspace/main.clj
@@ -14,20 +14,23 @@
 
 (defn -main [& command_args]
   (let [[options args banner] (cli command_args
-    ["-h" "--help" "Displays help." :default false]
+    ["-h" "--help" "Displays help." :flag true :default false]
     ["-p" "--provider" "the file hosting provider" :default 'dropbox]
     ["-i" "--init" "initializes hackspace" :default false, :flag true]
     )]
-    (if (:help options)
+    (when (:help options)
       (println banner)
+      (System/exit 0))
+
+    (if (:init options)
       (do
         (cond (true? (:init options)) (initialize))
         )
       )
-    (println options)
     )
   )
 
 
+(initialize)
 
 
