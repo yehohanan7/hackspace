4f99667ca8786405e50f9aa3fd08b732fc3b6678
John Pradeep <yehohanan7@gmail.com>
2012-07-01 05:37:48 +0530

    user info is fetched

--- src/com/hackspace/oauth.clj
+++ src/com/hackspace/oauth.clj
@@ -17,18 +17,6 @@
 (def consumersecret "q5oemkk7d2muvxp")
 
 
-;;(def SHA "HmacSHA1")
-;;(def ENCODE "UTF-8")
-
-
-
-;;(defn generate-signature [url method params]
- ;; (let [mac (Mac/getInstance SHA) base (str method "&" url "&" params) key (SecretKeySpec. (.getBytes (str consumersecret "&") ENCODE) SHA)]
- ;;   (.init mac key)
- ;;   (String. (.encode (new Base64) (.doFinal mac (.getBytes base ENCODE))) ENCODE)
- ;;   )
- ;; )
-
 (defn http-get [url token secret params]
   (let [consumer (CommonsHttpOAuthConsumer. consumerkey consumersecret) http-client (new DefaultHttpClient) get-request (new HttpGet url)]
     (. consumer setTokenWithSecret token secret)
@@ -39,4 +27,4 @@
   )
 
 
-(println (http-get "https://api.dropbox.com/1/account/info" "dvidfhemn0w9zt8" "2y4r0jubhm9hils"  {}))
+(println (:display_name (http-get "https://api.dropbox.com/1/account/info" "" ""  {})))
